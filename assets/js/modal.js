a35fae678113ecb3a73f24485a2b6de18aa24952
Lovely Tautuaa <lovelytau650@gmail.com>
2024-03-19 21:23:28 -0700

    Added js to modal button

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,11 @@
 const logoButton = document.querySelector('#hawaiiPopUp');
-const modalBG = document.querySelector('.modalBackground');
+const modalBG = document.querySelector('.modal-background');
 const modal = document.querySelector('.modal');
+
+logoButton.addEventListener('click', function(){
+    modal.classList.add('is-active');
+});
+
+modalBG.addEventListener('click', function(){
+    modal.classList.remove('is-active');
+})
